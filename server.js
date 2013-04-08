c0ea91101bb0ff7604a4084bb51afd1ca6f37c07
Clement Ho <chh09a@acu.edu>
2013-04-07 19:51:49 -0500

    One more test for CI system

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World Test Change!');
+  response.send('Hello World Test Change x2!');
 });
 
 var port = process.env.PORT || 5000;
